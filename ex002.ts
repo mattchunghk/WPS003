22a087c0b3ea764bc08b659d4551978f96cb63f6
mattchunghk <mattchunghk@gmail.com>
2022-08-28 17:58:36 +0800

    last

--- ex002.ts
+++ ex002.ts
@@ -18,41 +18,26 @@ import fs from 'fs';
 //     })   
 // }
 
-function listAllJsRecursive2 (path:string){
-    return new Promise((resolve,reject)=>{
-        let files = fs.readdirSync(path)
-        for (let file of files){
-           fs.stat(`${path}/${file}`, (error, stats)=>{if(error) reject(error);
-            if(stats.isDirectory()){
-                listAllJsRecursive2 (`${path}/${file}`)
-            }else{
-                let filePath =  file.split('.').pop()
-                 if(filePath == "ts"){
-                resolve(console.log(path+file));   
-
-            }
-        }
+async function listAllJsRecursive (path:string){ 
+   try {
+      const files = await fs.promises.readdir(path)
+      files.map(async file=>{
+        let stat = await fs.promises.stat(`${path}/${file}`)
+        if(stat.isDirectory()){
+            listAllJsRecursive(`${path}/${file}`)
+        }else{
+            let filePath = file.split('.').pop()
+            if(filePath == "ts"){
+            console.log(path+file)  
+            }}
         })
-           
-        }
- 
-    })
 
+   }catch(err) {
+    console.log(err)
+   }
 }
 
 
 
 
-
-//  let listAllJsRecursive = async (path:string) =>
-//     await Promise.all((await fs.promises.readdir(path)).map(async file =>
-//        (await fs.promises.stat(`${path}/${file}`)).isDirectory() ?
-//        listAllJsRecursive(`${path}/${file}`) : console.log(file)
-//     ))
-
-
-
-
-
-listAllJsRecursive2('/Users/mattchung/Desktop/c22/WSP/WSP003/');
-
+listAllJsRecursive('/Users/mattchung/Desktop/c22/WSP/WSP003');
